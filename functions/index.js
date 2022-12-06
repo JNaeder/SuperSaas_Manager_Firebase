a4a4d9ea273d5188b570214a06dc2e088ca9bdf6
JNaeder <39416919+JNaeder@users.noreply.github.com>
2022-12-05 21:24:22 -0500

    Just in case

--- functions/index.js
+++ functions/index.js
@@ -1,45 +1,18 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const googleSheets = require("./myScripts/googleSheets");
+
 admin.initializeApp();
 const db = admin.firestore().collection("students");
 
-const creds = require("./google_credentials.json");
-const { GoogleSpreadsheet } = require("google-spreadsheet");
-const doc = new GoogleSpreadsheet(
-  "1v1d2RHcofxOYnUU5m7SDgymvIU160RUvpsaO0r1MNCM"
-);
-
-exports.newuser = functions.https.onRequest(async (req, res) => {
-  const data = req.body;
-  await db.add(data);
-  const outputString = `${data.name} has been added to the DB`;
-  res.status(200).send(outputString);
-});
-
 exports.sheetinfo = functions.https.onRequest(async (req, res) => {
-  await doc.useServiceAccountAuth(creds);
-  await doc.loadInfo();
-  const sheet = doc.sheetsByIndex[0];
-  const allRows = await sheet.getRows();
-
-  for (let i = 0; i < allRows.length; i++) {
-    // Get Student Info
-    const studentID = allRows[i]["StuNum"];
-    const studentName = allRows[i]["Student Name"];
-
-    // Check them in the DB
-    const studentByID = await db.where("studentID", "==", studentID).get();
-    const studentDoesNotExist = studentByID.empty;
-
-    // Update/Add them
-    if (studentDoesNotExist) {
-      // Add student to the database
-      console.log(`Add ${studentName}`);
-    } else {
-      // Check the GPA/ICR/Mod/Class and update them if necessary
-      console.log(`Update ${studentName}`);
-    }
-  }
-
+  googleSheets.getStudentDataFromGoogleSheets(db);
   res.status(200).send("Done");
 });
+
+exports.getSheetInfoSchedule = functions.pubsub
+  .schedule("* * * * *")
+  .onRun(() => {
+    console.log(`Hello at ${new Date().getTime()}`);
+    return null;
+  });
---
+++ functions/myScripts/googleSheets.js
@@ -0,0 +1,43 @@
+const creds = require("../google_credentials.json");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const doc = new GoogleSpreadsheet(
+  "1v1d2RHcofxOYnUU5m7SDgymvIU160RUvpsaO0r1MNCM"
+);
+
+async function getStudentDataFromGoogleSheets(db) {
+  // Setup the Google Sheets
+  await doc.useServiceAccountAuth(creds);
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[0];
+  const allStudents = await sheet.getRows();
+
+  // Loop through all the students in the Google Sheet
+  for (let i = 0; i < allStudents.length; i++) {
+    // Get Student Info
+    const studentID = allStudents[i]["StuNum"];
+    const studentName = allStudents[i]["Student Name"];
+    const studentNameFirst = studentName.split(", ")[1];
+    const studentNameLast = studentName.split(", ")[0];
+    const studentMod = allStudents[i]["Mod Section"];
+    const currentInstructor = allStudents[i]["Instructor"];
+    const gpa = parseFloat(allStudents[i]["Projected GPA"]);
+    const icr = parseFloat(allStudents[i]["Running ICR"]);
+
+    // Create Student Object
+    const newStudentObject = {
+      firstName: studentNameFirst,
+      lastName: studentNameLast,
+      studentID: studentID,
+      mod: studentMod,
+      instructor: currentInstructor,
+      gpa: gpa,
+      icr: icr,
+    };
+
+    // Add it to the database
+    db.doc(studentID).set(newStudentObject);
+  }
+  console.log("All Students have been processed");
+}
+
+exports.getStudentDataFromGoogleSheets = getStudentDataFromGoogleSheets;
