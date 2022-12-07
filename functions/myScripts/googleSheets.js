aecc75a8df312093d247533113f869e7da5607e4
JNaeder <J.naeder324@gmail.com>
2022-12-07 13:37:07 -0500

    Fixing deployed firestore issue

--- functions/myScripts/googleSheets.js
+++ functions/myScripts/googleSheets.js
@@ -15,42 +15,42 @@ async function getStudentDataFromGoogleSheets(db, logger) {
   const allStudents = await sheet.getRows();
 
   const qSnapshot = await rawStudentDB.get();
-  const academicStudents = qSnapshot.docs.map((docSnapshot) => {
-    return docSnapshot.data();
-  });
+  // const academicStudents = qSnapshot.docs.map((docSnapshot) => {
+  //   return docSnapshot.data();
+  // });
 
   // Loop through all the students in the Google Sheet
   for (let i = 0; i < allStudents.length; i++) {
     // Get Student Info
     const studentID = allStudents[i]["StuNum"];
-    const academicFile = academicStudents.filter(
-      (student) => student["studentID"] === studentID
-    );
-    console.log(academicFile);
+    // const academicFile = academicStudents.filter(
+    //   (student) => student["studentID"] === studentID
+    // );
+    // console.log(academicFile);
 
-    // const studentName = allStudents[i]["Student Name"];
-    // const studentNameFirst = studentName.split(", ")[1];
-    // const studentNameLast = studentName.split(", ")[0];
-    // const studentMod = allStudents[i]["Mod Section"];
-    // const currentInstructor = allStudents[i]["Instructor"];
-    // const gpa = parseFloat(allStudents[i]["Projected GPA"]);
-    // const icr = parseFloat(allStudents[i]["Running ICR"]);
+    const studentName = allStudents[i]["Student Name"];
+    const studentNameFirst = studentName.split(", ")[1];
+    const studentNameLast = studentName.split(", ")[0];
+    const studentMod = allStudents[i]["Mod Section"];
+    const currentInstructor = allStudents[i]["Instructor"];
+    const gpa = parseFloat(allStudents[i]["Projected GPA"]);
+    const icr = parseFloat(allStudents[i]["Running ICR"]);
 
     // Create Student Object
-    //   const newStudentObject = {
-    //     firstName: studentNameFirst,
-    //     lastName: studentNameLast,
-    //     studentID: studentID,
-    //     mod: studentMod,
-    //     instructor: currentInstructor,
-    //     gpa: gpa,
-    //     icr: icr,
-    //   };
+    const newStudentObject = {
+      firstName: studentNameFirst,
+      lastName: studentNameLast,
+      studentID: studentID,
+      mod: studentMod,
+      instructor: currentInstructor,
+      gpa: gpa,
+      icr: icr,
+    };
 
-    //   // Add it to the database
-    //   rawStudentDB.doc(studentID).set(newStudentObject);
-    //   // console.log(db.doc(studentID));
-    //   logger.debug(`${studentName} Grade:${gpa}`);
+    // Add it to the database
+    const newDoc = await rawStudentDB.doc(studentID).set(newStudentObject);
+    const writeTime = newDoc.writeTime.toDate();
+    logger.debug(`${studentName} updated at ${writeTime}`);
   }
   return "All Students have been processed";
 }
