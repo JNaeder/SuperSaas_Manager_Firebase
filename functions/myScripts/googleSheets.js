078b5b9bae2d8f604812fc6b4c799822ac5aa353
JNaeder <J.naeder324@gmail.com>
2022-12-07 09:43:45 -0500

    Just a little bit more

--- functions/myScripts/googleSheets.js
+++ functions/myScripts/googleSheets.js
@@ -14,33 +14,43 @@ async function getStudentDataFromGoogleSheets(db, logger) {
   const sheet = doc.sheetsByIndex[0];
   const allStudents = await sheet.getRows();
 
+  const qSnapshot = await rawStudentDB.get();
+  const academicStudents = qSnapshot.docs.map((docSnapshot) => {
+    return docSnapshot.data();
+  });
+
   // Loop through all the students in the Google Sheet
   for (let i = 0; i < allStudents.length; i++) {
     // Get Student Info
     const studentID = allStudents[i]["StuNum"];
-    const studentName = allStudents[i]["Student Name"];
-    const studentNameFirst = studentName.split(", ")[1];
-    const studentNameLast = studentName.split(", ")[0];
-    const studentMod = allStudents[i]["Mod Section"];
-    const currentInstructor = allStudents[i]["Instructor"];
-    const gpa = parseFloat(allStudents[i]["Projected GPA"]);
-    const icr = parseFloat(allStudents[i]["Running ICR"]);
+    const academicFile = academicStudents.filter(
+      (student) => student["studentID"] === studentID
+    );
+    console.log(academicFile);
+
+    // const studentName = allStudents[i]["Student Name"];
+    // const studentNameFirst = studentName.split(", ")[1];
+    // const studentNameLast = studentName.split(", ")[0];
+    // const studentMod = allStudents[i]["Mod Section"];
+    // const currentInstructor = allStudents[i]["Instructor"];
+    // const gpa = parseFloat(allStudents[i]["Projected GPA"]);
+    // const icr = parseFloat(allStudents[i]["Running ICR"]);
 
     // Create Student Object
-    const newStudentObject = {
-      firstName: studentNameFirst,
-      lastName: studentNameLast,
-      studentID: studentID,
-      mod: studentMod,
-      instructor: currentInstructor,
-      gpa: gpa,
-      icr: icr,
-    };
+    //   const newStudentObject = {
+    //     firstName: studentNameFirst,
+    //     lastName: studentNameLast,
+    //     studentID: studentID,
+    //     mod: studentMod,
+    //     instructor: currentInstructor,
+    //     gpa: gpa,
+    //     icr: icr,
+    //   };
 
-    // Add it to the database
-    rawStudentDB.doc(studentID).set(newStudentObject);
-    // console.log(db.doc(studentID));
-    logger.debug(`${studentName} Grade:${gpa}`);
+    //   // Add it to the database
+    //   rawStudentDB.doc(studentID).set(newStudentObject);
+    //   // console.log(db.doc(studentID));
+    //   logger.debug(`${studentName} Grade:${gpa}`);
   }
   return "All Students have been processed";
 }
--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const functions = getFunctions(app);
-// connectFunctionsEmulator(functions, "localhost", 5001);
+connectFunctionsEmulator(functions, "localhost", 5001);
 
 const getGoogleSheetInfo = async () => {
   console.log("Starting");
