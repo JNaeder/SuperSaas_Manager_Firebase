58a028e5abeaedb40ed1740541fe30757521759b
JNaeder <39416919+JNaeder@users.noreply.github.com>
2022-12-28 23:17:15 -0500

    also worked on today booking layout

--- src/components/TodayBookingContainer.js
+++ src/components/TodayBookingContainer.js
@@ -5,6 +5,39 @@ function TodayBookingContainer({ todayBookings }) {
   return (
     <>
       <div className="booking_container">
+        <h4 style={{ gridColumn: 2 }} className="booking_title">
+          Suite 1
+        </h4>
+        <h4 style={{ gridColumn: 3 }} className="booking_title">
+          Suite 2
+        </h4>
+        <h4 style={{ gridColumn: 4 }} className="booking_title">
+          Suite 3
+        </h4>
+        <h4 style={{ gridColumn: 5 }} className="booking_title">
+          Suite 4
+        </h4>
+        <h4 style={{ gridColumn: 6 }} className="booking_title">
+          Audient
+        </h4>
+        <h4 style={{ gridColumn: 7 }} className="booking_title">
+          02R
+        </h4>
+        <h4 style={{ gridColumn: 8 }} className="booking_title">
+          SSL
+        </h4>
+        <h4 style={{ gridColumn: 9 }} className="booking_title">
+          S6
+        </h4>
+        <h4 style={{ gridColumn: 10 }} className="booking_title">
+          Neve
+        </h4>
+        <h4 style={{ gridRow: "2 / span 2" }}>10 am</h4>
+        <h4 style={{ gridRow: "4 / span 2" }}>12 pm</h4>
+        <h4 style={{ gridRow: "6 / span 2" }}>2 pm</h4>
+        <h4 style={{ gridRow: "8 / span 2" }}>4 pm</h4>
+        <h4 style={{ gridRow: "10 / span 2" }}>6 pm</h4>
+        <h4 style={{ gridRow: "12 / span 2" }}>8 pm</h4>
         {todayBookings.map((booking, i) => (
           <TodayBookingFile booking={booking} key={i} />
         ))}
--- src/components/TodayBookingFile.js
+++ src/components/TodayBookingFile.js
@@ -1,7 +1,7 @@
 import moment from "moment/moment";
+import { MdCheckCircle, MdDangerous } from "react-icons/md";
 
 const chooseColor = (res_name) => {
-  const base = "todayBooking_file ";
   if (res_name.includes("Suite")) {
     return "#5e35b1";
   } else if (res_name.includes("Audient")) {
@@ -12,20 +12,80 @@ const chooseColor = (res_name) => {
     return "#d81b60";
   } else if (res_name.includes("SSL")) {
     return "#01acc1";
+  } else if (res_name.includes("S6")) {
+    return "#f9a825";
   }
 };
 
+const getColumn = (res_name) => {
+  switch (res_name) {
+    case "Production Suite 1":
+      return 2;
+    case "Production Suite 2":
+      return 3;
+    case "Production Suite 3":
+      return 4;
+    case "Production Suite 4":
+      return 5;
+    case "Audient":
+      return 6;
+    case "02R":
+      return 7;
+    case "SSL":
+      return 8;
+    case "Avid S6":
+      return 9;
+    case "Neve":
+      return 10;
+  }
+};
+
+const bookingCheck = (booking) => {
+  console.log(`${booking["student_name"]} showed up and is good to go.`);
+};
+
+const bookingBlock = (booking) => {
+  console.log(
+    `${booking["student_name"]} did not show up to their ${booking["studio"]} booking`
+  );
+};
+
 function TodayBookingFile({ booking }) {
-  const { studio, studentName } = booking;
-  const bgColor = chooseColor(studio);
+  const { studio, student_name, start_time, end_time, student_email } = booking;
+  const color = chooseColor(studio);
+  const column = getColumn(studio);
+  const rowStart = moment(start_time).hour() - 8;
+  const rowEnd = moment(end_time).hour() - 8;
+  const emailEnding = student_email.split("@")[1];
   const gridStyle = {
-    backgroundColor: bgColor,
-    gridColumn: "1 / 2",
-    gridRow: "1 / 2",
+    backgroundColor: color,
+    gridColumn: `${column} / span 1`,
+    gridRow: `${rowStart} / ${rowEnd}`,
   };
   return (
     <>
-      <div style={gridStyle}>{studentName}</div>
+      <div style={gridStyle} className="booking">
+        <h4 className="booking_student_name">{student_name}</h4>
+
+        {emailEnding === "saeinstitute.edu" ? (
+          <div className="booking_buttons_container">
+            <button
+              className="booking_button good"
+              onClick={() => bookingCheck(booking)}
+            >
+              <MdCheckCircle />
+            </button>
+            <button
+              className="booking_button bad"
+              onClick={() => bookingBlock(booking)}
+            >
+              <MdDangerous />
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </>
   );
 }
